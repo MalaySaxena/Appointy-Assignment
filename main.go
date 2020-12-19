25f06298fa5448a3c71d8c5c2a0b68eee58c4d70
MalaySaxena <dynamic.malay@gmail.com>
2020-12-19 12:57:28 +0530

    contact model added

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import(
 	"log"
 	"net/http"
 
-	"github.com/faygun/go-rest-api/helper"
-	"github.com/faygun/go-rest-api/models"
+	"helper"
+	"models"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -16,13 +16,33 @@ import(
 
 var collection = helper.ConnectDB()
 
+func getUser(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var user models.user
+	var params = mux.Vars(r)
+
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+
+	filter := bson.M{"id": id}
+	err := collection.FindOne(context.TODO(), filter).Decode(&use)
+
+	if err != nil {
+		helper.GetError(err, w)
+		return
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
 func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api/users/{id}", getUser).Methods("GET")
 	r.HandleFunc("/api/users", createUser).Methods("POST")
 	r.HandleFunc("/contacts", newContact).Methods("POST")
-	//r.HandleFunc("/contacts", newContact).Methods("POST")
+	r.HandleFunc("/contacts", newContact).Methods("POST")
 
 	config := helper.GetConfiguration()
 	log.Fatal(http.ListenAndServe(config.Port, r))
